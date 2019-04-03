b83f6dd81ceabf7f172f0c4bf255c5f712372e36
Dhruvika Sahni <dhruvika@mit.edu>
2019-04-03 17:32:37 -0400

    base web app

---
+++ home.js
@@ -0,0 +1,11 @@
+
+
+Util.events(document, {
+	// Final initalization entry point: the Javascript code inside this block
+	// runs at the end of start-up when the DOM is ready
+
+	"DOMContentLoaded": function(e) {
+		// want to load sections and classes list
+		}
+
+});
