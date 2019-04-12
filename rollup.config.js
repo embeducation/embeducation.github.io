2acafb971c94cb6b4e791f6a5a4628f84486a5f8
Dhruvika Sahni <dhruvika@mit.edu>
2019-04-12 16:42:44 -0400

    Added viz to web app YAY

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import {terser} from 'rollup-plugin-terser';
+import buble from 'rollup-plugin-buble';
+import path from 'path';
+
+import {main} from './package.json';
+
+let globals = {
+	'three': 'THREE',
+	'three.texttexture': 'THREE.TextTexture',
+};
+
+export default {
+	input: 'src/index.js',
+	external: Object.keys(globals),
+	output: {
+		file: main,
+		format: 'umd',
+		name: path.basename(main, path.extname(main)),
+		globals,
+	},
+	plugins: [
+		buble({objectAssign: 'Object.assign'}),
+		terser(),
+	],
+};
