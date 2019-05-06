d681a76b12c5a3398bb5bd158ffa1211885d3961
srilayab <srilayab@mit.edu>
2019-05-06 16:05:11 -0400

    Changed Harry to Orange, uploaded labels and vectors-full-book3 (things not searching in right category)

--- tsv_to_csv.py
+++ tsv_to_csv.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
 
-tsv_file='vectors-book3-cleaned.tsv'
+tsv_file='vectors-full-book3.tsv'
 csv_table=pd.read_table(tsv_file,sep='\t')
-csv_table.to_csv('vectors-book3-cleaned.csv',index=False)
+csv_table.to_csv('vectors-full-book3.csv',index=False)
