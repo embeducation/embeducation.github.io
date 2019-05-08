21b4b3051a2ecc51addbd5b834e298863da47a90
Dhruvika Sahni <dhruvika@mit.edu>
2019-05-07 21:53:40 -0400

    Added the get to know the characters option

--- home.js
+++ home.js
@@ -633,6 +633,28 @@ function findPath(from_word, to_word) {
 	showVizualization(main_word = null, targetValues = path, pathWords = path_words);
 }
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
 function findAnalogy(a1, a2, b1) {
 	analogy_result = ""
 
